72a304a1c83fc909053b3ee61f445b9b397696ad
kasinathraghavendra <147577941+kasinathraghavendra@users.noreply.github.com>
2023-11-01 12:03:31 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <div>
+        <h1 className="heading"> Super Over League</h1>
+        <div className="imgcontainer">
+          <img
+            src="https://assets.ccbp.in/frontend/react-js/rcb-img.png"
+            alt="rcb-img"
+            className="img"
+          />
+
+          <img
+            src="https://assets.ccbp.in/frontend/react-js/csk-img.png"
+            alt="csk-img"
+            className="img"
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
